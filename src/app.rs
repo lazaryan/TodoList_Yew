1a67f6885b6faa508bac42bb08755830ace5eb14
lazaryan <lazaryan99@gmail.com>
2022-02-01 22:53:39 +0300

    add input tasks and view its

--- src/app.rs
+++ src/app.rs
@@ -1,24 +1,32 @@
 use gloo::storage::{LocalStorage, Storage};
-use yew::prelude::*;
+use web_sys::HtmlInputElement as InputElement;
+use yew::{
+    classes,
+    events::{FocusEvent, KeyboardEvent},
+    html,
+    html::Scope,
+    Classes, Component, Context, Html, NodeRef, TargetCast,
+};
 
-use crate::state::{State, Filter};
+use crate::state::{State, Filter, Entry};
 
 pub enum Msg {
-    AddOne,
+    Add(String),
+    SetFilter(Filter),
 }
 
 pub struct App {
     state: State,
 }
 
-const STORE_KEY: &str = "yew.todomvc.self";
+const STORAGE_KEY: &str = "yew.todomvc.self";
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        let entries = LocalStorage::get(STORE_KEY).unwrap_or_else(|_| Vec::new());
+        let entries = LocalStorage::get(STORAGE_KEY).unwrap_or_else(|_| Vec::new());
         let state = State {
             entries,
             filter: Filter::All,
@@ -30,15 +38,111 @@ impl Component for App {
         }
     }
 
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Add(description) => {
+                if !description.is_empty() {
+                    let entry = Entry {
+                        description: description.trim().to_string(),
+                        completed: false,
+                        editing: false,
+                    };
+                    self.state.entries.push(entry);
+                }
+            }
+            Msg::SetFilter(filter) => {
+                self.state.filter = filter;
+            }
+        }
+
+        LocalStorage::set(STORAGE_KEY, &self.state.entries).expect("failed to set");
+        true
+    }
+
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let hidden_class = if self.state.entries.is_empty() {
+            "hidden"
+        } else {
+            ""
+        };
+
         html! {
             <div class="todomvc-wrapper">
                 <section class="todoapp">
                     <header class="header">
                         <h1>{ "todos" }</h1>
+                        { self.view_input(ctx.link()) }
                     </header>
+                    <section class={classes!("main", hidden_class)}>
+                        <ul class="todo-list">
+                            {
+                                for self.state.entries
+                                    .iter()
+                                    .filter(|e| self.state.filter.fits(e))
+                                    .enumerate()
+                                    .map(|e| self.view_entry(e, ctx.link()))
+                            }
+                        </ul>
+                    </section>
+                    <footer class={classes!("footer", hidden_class)}>
+                        <span class="todo-count">
+                            <strong>{ self.state.total() }</strong>
+                            { " item(s) left" }
+                        </span>
+                    </footer>
                 </section>
+                <footer class="info">
+                    <p>{ "Double-click to edit a todo" }</p>
+                    <p>{ "Written by " }<a href="https://github.com/lazaryan" target="_blank">{ "Sergey Lazaryan" }</a></p>
+                    <p>{ "Part of " }<a href="http://todomvc.com/" target="_blank">{ "TodoMVC" }</a></p>
+                </footer>
             </div>
+        }
+    }
+}
+
+impl App {
+    fn view_input(&self, link: &Scope<Self>) -> Html {
+        let onkeypress = link.batch_callback(|e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let input: InputElement = e.target_unchecked_into();
+                let value = input.value();
+                input.set_value("");
+                Some(Msg::Add(value))
+            } else {
+                None
+            }
+        });
+
+        html! {
+            <input
+                class="new-todo"
+                placeholder="What needs to be done?"
+                {onkeypress}
+            />
+        }
+    }
+
+    fn view_entry(&self, (idx, entry): (usize, &Entry), link: &Scope<Self>) -> Html {
+        let mut class = Classes::from("todo");
+        if entry.editing {
+            class.push(" editing");
+        }
+        if entry.completed {
+            class.push(" completed");
+        }
+        html! {
+            <li {class}>
+                <div class="view">
+                    <input
+                        type="checkbox"
+                        class="toggle"
+                        checked={entry.completed}
+                    />
+                    <label>{ &entry.description }</label>
+                    <button class="destroy" />
+                </div>
+            </li>
         }
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -12,6 +12,20 @@ impl State {
     pub fn total(&self) -> usize {
         self.entries.len()
     }
+
+    pub fn is_all_completed(&self) -> bool {
+        let mut filtered_iter = self
+            .entries
+            .iter()
+            .filter(|e| self.filter.fits(e))
+            .peekable();
+
+        if filtered_iter.peek().is_none() {
+            return false;
+        }
+
+        filtered_iter.all(|e| e.completed)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -26,4 +40,22 @@ pub enum Filter {
     All,
     Active,
     Completed,
+}
+
+impl Filter {
+    pub fn fits(&self, entry: &Entry) -> bool {
+        match *self {
+            Filter::All => true,
+            Filter::Active => !entry.completed,
+            Filter::Completed => entry.completed,
+        }
+    }
+
+    pub fn as_href(&self) -> &'static str {
+        match self {
+            Filter::All => "#/",
+            Filter::Active => "#/active",
+            Filter::Completed => "#/completed",
+        }
+    }
 }
